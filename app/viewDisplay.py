5819c2f7cf022f45bde3c6925b4215e7902b9e6f
Joe Gakumo <joegakumo1@gmail.com>
2021-09-09 21:58:18 +0300

    add templates files

--- app/viewDisplay.py
+++ app/viewDisplay.py
@@ -7,4 +7,34 @@ def index():
     '''
     ViewDisplay root page function that retruns the index page and its data
     '''
-    return render_template('index.html')
+    landing="Welcome to Newspaper Application "
+    return render_template('index.html',landing=landing)
+
+@app.route('/bussiness')  
+def bussiness():
+    '''
+    
+    '''  
+    
+    return render_template('bussiness.html',)
+
+@app.route('/technology')
+def technology():
+    '''
+    
+    '''  
+    title="Technology" 
+    return render_template('technology.html',) 
+@app.route('/sports')
+def sports():
+    '''
+    
+    '''   
+    return render_template('sports.html',)
+@app.route('/entertain')
+def entertain():
+    '''
+    
+    '''     
+    return render_template('entertain.html')
+
